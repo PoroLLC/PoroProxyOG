cf09e14a7d48684ccfdb0a60aa8dbc07fb6031e9
AnthonyPlayzYT <75546096+AnthonyPlayzYT@users.noreply.github.com>
2022-02-06 17:49:20 -0500

    Add files via upload

---
+++ Sub Bot 1/run.bat
@@ -0,0 +1,15 @@
+@echo off
+echo PoroProxy - Created by AntPlayz and LavaKid.
+echo.
+echo.
+echo STARTING BOT 2... After this starts, go into the last sub folder and run all the batch file.
+echo Keep this open if you want your bot to stay online.
+echo.
+node index2.js
+
+
+
+
+
+
+pause
---
+++ Sub Bot 2/run.bat
@@ -0,0 +1,15 @@
+@echo off
+echo PoroProxy - Created by AntPlayz and LavaKid.
+echo.
+echo.
+echo STARTING BOT 3... After this starts, have fun!
+echo Keep this open if you want your bot to stay online.
+echo.
+node index3.js
+
+
+
+
+
+
+pause
